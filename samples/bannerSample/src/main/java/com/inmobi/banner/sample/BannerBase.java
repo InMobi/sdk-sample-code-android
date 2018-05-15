e61b694178d21ffdacaf1f66d30d6815feb8f33c
Davendar Kumar Ojha <davendar.ojha@inmobi.com>
2018-05-15 17:55:46 +0530

    GDPR consent object

--- samples/bannerSample/src/main/java/com/inmobi/banner/sample/BannerBase.java
+++ samples/bannerSample/src/main/java/com/inmobi/banner/sample/BannerBase.java
@@ -10,12 +10,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class BannerBase extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        InMobiSdk.init(this, "1234567890qwerty0987654321qwerty12345");
+        JSONObject consent = new JSONObject();
+        try {
+            // Provide correct consent value to sdk which is obtained by User
+            consent.put(InMobiSdk.IM_GDPR_CONSENT_AVAILABLE, true);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        InMobiSdk.init(this, "123456789abcdfghjiukljnm09874", consent);
         InMobiSdk.setLogLevel(InMobiSdk.LogLevel.DEBUG);
         setContentView(R.layout.banner_base);
 
